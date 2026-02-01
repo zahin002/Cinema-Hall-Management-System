52f4ed86a70811f98b15cb6f235418e764911ce1
faiyaZzz75 <faiyaztahmid@iut-dhaka.edu>
2026-02-01 19:19:22 +0600

    Update AdminService.h

--- src/AdminService.h
+++ src/AdminService.h
@@ -2,17 +2,29 @@
 #define ADMINSERVICE_H
 
 #include "User.h"
+#include <string>
+
+using namespace std;
 
 class AdminService {
 public:
+    // Main admin menu
     void adminMenu(const User& admin);
 
+    // Date validation for showtime (DD-MM-YYYY, today → next 30 days)
+    bool isValidFutureDate(const string& dateStr);
+
 private:
+    // Movie management
     void addMovie();
     void viewMovies();
+    void deleteMovie();
+
+    // Showtime management
     void addShowtime();
     void viewShowtimes();
-    void deleteMovie();
+
+    // Seat map management
     void createSeatMap();
     void viewSeatMap();
 };
